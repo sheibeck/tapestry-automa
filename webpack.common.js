8177cfce3cc2c516d1dac935b6220f7c14b6cb05
Sterling Heibeck <sterling.heibeck@nocti.org>
2019-09-18 16:00:25 -0400

    configure environments

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/app.js'
+  },
+  module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+                options: {					
+                    outputPath: 'images',
+                    name: '[name].[ext]',
+                },
+            },
+        ]
+    },    
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
